0127a28833724ef6bec6893c6e7f94373677abc0
TaoJianping <tjp563613470@live.com>
2019-12-23 01:07:15 +0800

    update

---
+++ Books/程序员的自我修养/code/pthread.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int pthread_create(
+    pthread_t*,
+        const pthread_attr_t*,
+        void* (*)(void*),
+        void*) __attribute__ ((weak));
+
+int main()
+{
+    if (pthread_create) {
+        printf("This is multi-thread version! \n");
+    } else {
+        printf("This is single-thread version!\n");
+    }
+}
