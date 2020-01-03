22f02b6e349fe5f038225625dcc2ebcffe3ee876
TaoJianping <tjp563613470@live.com>
2020-01-03 21:44:41 +0800

    update chapter 4 6 7

---
+++ Books/程序员的自我修养/code/target.c
@@ -0,0 +1,12 @@
+/* target.c */
+#include <stdio.h>
+#include "bfd.h"
+
+int main() {
+    const char** t = bfd_target_list();
+    while (*t)
+    {
+        printf("%s\n", *t);
+        t++;
+    }
+}
